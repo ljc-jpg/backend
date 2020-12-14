339b722bd84e024986d500691d2da8272f51789c
朱铮 <15850595763@sina.cn>
2020-12-14 14:13:45 +0800

    登录登出

--- cas-server/src/main/java/com/cloud/controller/ArticleController.java
+++ cas-server/src/main/java/com/cloud/controller/ArticleController.java
@@ -1,12 +1,21 @@
 package com.cloud.controller;
 
+import com.cloud.model.Article;
+import com.cloud.model.User;
+import com.cloud.service.ArticleService;
+import com.cloud.util.ResultVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.cloud.util.ResultVo.RETURN_CODE_ERR;
+
 /**
- *
  * @author zhuz
  * @date 2020/12/7
  */
@@ -14,10 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/article")
 public class ArticleController {
 
+
     private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
+    @Resource
+    private ArticleService articleService;
 
-
+    @GetMapping("/articleByUser")
+    public ResultVo<Article> articleByUser(Article article) {
+        ResultVo resultVo = new ResultVo();
+        try {
+            List<Article> list  = articleService.articleByUser(article);
+            resultVo.setData(list);
+        } catch (Exception e) {
+            logger.error("articleByUser:", e);
+            resultVo.setMsg("articleByUser:" + e);
+            resultVo.setCode(RETURN_CODE_ERR);
+        }
+        return resultVo;
+    }
 
 
 }
---
+++ cas-server/src/main/java/com/cloud/service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.cloud.service;
+
+import com.cloud.model.Article;
+import com.cloud.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author zhuz
+ * @Date 2020/12/14
+ */
+@Service
+public class ArticleService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArticleService.class);
+
+    public List<Article> articleByUser(Article article) {
+
+
+
+
+        return null;
+    }
+
+}
